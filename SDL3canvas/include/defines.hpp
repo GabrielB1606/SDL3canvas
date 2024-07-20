f914d9032a661a110d2488dfee059f117b17460f
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-07-20 11:44:50 -0400

    click to insert points

--- SDL3canvas/include/defines.hpp
+++ SDL3canvas/include/defines.hpp
@@ -1,3 +1,37 @@
 #ifndef DEFINES_HPP
 #define DEFINES_HPP
+
+#include <vector>
+#include <SDL3/SDL.h>
+
+struct FCircle{
+
+  float cx, cy, r;
+  std::vector<SDL_FPoint> points;
+
+  FCircle(float cx, float cy, float r){
+    this->cx = cx;
+    this->cy = cy;
+    this->r = r;
+
+    for (float i = -r; i <= r; i += 0.5){
+      float x = SDL_sqrt(r * r - i * i);
+      points.push_back(SDL_FPoint{cx - x, cy + i});
+      points.push_back(SDL_FPoint{cx + x, cy + i});
+    }
+  }
+
+  void render(SDL_Renderer *renderer) const {
+    if (!renderer)
+      return;
+    SDL_RenderLines(renderer, points.data(), points.size());
+  }
+};
+
+struct FQuadtree{
+
+  int capacity;
+
+};
+
 #endif
--- SDL3engine/src/main.cpp
+++ SDL3engine/src/main.cpp
@@ -1,36 +1,7 @@
-#include <stdio.h>
-#include <vector>
 #include "include/defines.hpp"
-#include <SDL3/SDL.h>
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-
-struct FCircle{
-
-  float cx, cy, r;
-  std::vector<SDL_FPoint> points;
-
-  FCircle(float cx, float cy, float r){
-    this->cx = cx;
-    this->cy = cy;
-    this->r = r;
-
-    for (float i = -r; i <= r; i+=0.5){
-      float x = SDL_sqrt(r*r - i*i);
-      points.push_back( SDL_FPoint{cx-x, cy+i} );
-      points.push_back( SDL_FPoint{cx+x, cy+i} );
-    }
-
-  }
-
-  void render(SDL_Renderer* renderer){
-    if(!renderer)
-      return;
-    SDL_RenderLines(renderer, points.data(), points.size());
-  }
-
-};
 
 int main(int, char**){
   if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_EVENTS|SDL_INIT_VIDEO) != 0){
@@ -42,13 +13,7 @@ int main(int, char**){
   renderer = SDL_CreateRenderer(window, NULL);
   bool running = 1;
   
-  SDL_FRect test = {100, 100, 50, 50};
-  SDL_Vertex geom[3] = {
-    {SDL_FPoint{0,0}, SDL_FColor{0xff,0,0,0xff}, SDL_FPoint{0,0}},
-    {SDL_FPoint{100,0}, SDL_FColor{0,0,0xff,0xff}, SDL_FPoint{1,0}},
-    {SDL_FPoint{0,100}, SDL_FColor{0,0xff,0,0xff}, SDL_FPoint{0,1}}
-  };
-  int idxs[3] = {0,1,2};
+  std::vector<FCircle> pts;
 
   FCircle circ(200, 200, 100);
 
@@ -59,20 +24,21 @@ int main(int, char**){
         case SDL_EVENT_QUIT:
           running = false;
           break;
+        case SDL_EVENT_MOUSE_BUTTON_DOWN:
+          pts.push_back( FCircle(event.motion.x, event.motion.y, 3) );
+          break;
         
         default:
           break;
       }
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0xff, 0xff);
+    SDL_SetRenderDrawColor(renderer, 69, 69, 69, 0xff);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0xff, 0, 0xff);
     SDL_SetRenderScale(renderer, 1, 1);
-    // SDL_RenderFillRect(renderer, &test);
-    // SDL_SetRenderScale(renderer, 2, 2);
-    // SDL_RenderPoint(renderer, 200,  200);
-    circ.render(renderer);
-    SDL_RenderLine(renderer, 0,0,500,500);
-    SDL_RenderGeometry(renderer, NULL, geom, 3, idxs, 3);
+    
+    for(const auto &c:pts)
+      c.render(renderer);
+      
     SDL_RenderPresent(renderer);
     SDL_Delay(1);
   }
