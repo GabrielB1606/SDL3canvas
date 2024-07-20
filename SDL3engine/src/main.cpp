cca793dc89cb691de7067cf85310ab2ef1c675ed
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-07-20 00:09:38 -0400

    cmake dll copy madness

--- SDL3engine/src/main.cpp
+++ SDL3engine/src/main.cpp
@@ -2,6 +2,33 @@
 #include "include/defines.hpp"
 #include <SDL3/SDL.h>
 
+SDL_Window* window;
+
 int main(int, char**){
-    printf("Hello, from SDL3canvas!!\n");
+
+  if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_EVENTS|SDL_INIT_VIDEO|SDL_INIT_TIMER|SDL_INIT_GAMEPAD) != 0){
+    SDL_Log("SDL failed to init!");
+    return -2;
+  }
+
+  window = SDL_CreateWindow("SDL3 Canvas", 1280, 720, 0);
+  bool running = 1;
+  
+  while(running){
+    SDL_Event event;
+    if(SDL_PollEvent(&event) > 0)
+      switch (event.type){
+        case SDL_EVENT_QUIT:
+          running = false;
+          break;
+        
+        default:
+          break;
+      }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
